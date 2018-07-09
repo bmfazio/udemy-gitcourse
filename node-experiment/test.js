3131ddef395d0d21094c2504437b60e11b0b7b13
bmfazio <boris.fazio@outlook.com>
2018-07-09 01:25:41 -0500

    jaja ya ps

--- node-experiment/test.js
+++ node-experiment/test.js
@@ -1,9 +1,17 @@
+// Ex 1
 console.log("Hello world!!!");
-
+// Ex 2
 var fs = require("fs");
+var https = require("https");
+
 fs.writeFile(__dirname + "/index.html","<h1>booyAh</h1>",function(error) {
 	if (error){
 		return console.log(error);
 	} else {
 		return console.log("todo gud B)");
 	}});
+var myPhotoLocation = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png";
+
+https.get(myPhotoLocation, function(response){
+	response.pipe(fs.createWriteStream(__dirname + "/img.png"));
+});
